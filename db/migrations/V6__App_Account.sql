7e62bf0ef0c3e47e2b125d0e70f93d1a255b644b
Evelyn Gordon <evelyn.gordon20@gmail.com>
2024-01-22 22:07:33 -0500

    Setup SQL app account

---
+++ db/migrations/V6__App_Account.sql
@@ -0,0 +1,6 @@
+CREATE USER AnimalTrackerApp FOR LOGIN AnimalTrackerApp
+
+GRANT EXECUTE ON ImportUser TO AnimalTrackerApp
+GRANT EXECUTE ON ImportOrganism TO AnimalTrackerApp
+GRANT EXECUTE ON ImportProject TO AnimalTrackerApp
+GRANT EXECUTE ON ImportSighting TO AnimalTrackerApp
