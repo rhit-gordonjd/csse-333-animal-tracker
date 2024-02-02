d8bde1aa57fa9c5ec4503494702d7e9c6ba5f9c3
Dot <stuhlmda@rose-hulman.edu>
2024-02-02 13:38:46 -0500

    added some stored procedures

---
+++ db/migrations/V17__Proc_Update_Username.sql
@@ -0,0 +1,35 @@
+-- Changes the user's display name
+CREATE PROCEDURE UpdateDisplayName
+(
+	@UserID int,
+	@NewUsername varchar(20)
+)
+AS 
+BEGIN
+--validate parameters
+    IF (@NewUsername IS NULL OR @NewUsername= '')
+    BEGIN
+        RAISERROR('Parameter @NewUsername is missing or empty', 14, 1);
+        RETURN 1;
+    END
+
+    IF (@UserID IS NULL OR @UserID= 0)
+    BEGIN
+        RAISERROR('Parameter @UserID is missing or empty', 14, 1);
+        RETURN 1;
+    END
+
+    IF (NOT EXISTS (SELECT * FROM [User] WHERE ID = @UserID))
+    BEGIN
+        RAISERROR('No user with that ID exists', 14, 2);
+        RETURN 2;
+    END
+
+	UPDATE [User]
+	SET Username = @NewUsername
+	WHERE ID = @UserID
+	RETURN 0;
+END
+GO
+
+GRANT EXECUTE ON UpdateUsername TO AnimalTrackerApp
---
+++ web/src/main/java/animaltracker/AnimalTrackerWeb/data/UserSettingsService.java
@@ -0,0 +1,81 @@
+package animaltracker.AnimalTrackerWeb.data;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Types;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserSettingsService {
+    @Autowired
+    private DataSource dataSource;
+
+    public boolean updateDisplayName(int userID, String newDisplayName) {
+        try {
+            Connection connection = dataSource.getConnection();
+            CallableStatement stmt = connection.prepareCall("{? = call UpdateDisplayName(@UserID = ?, @NewDisplayName = ?)}");
+            stmt.registerOutParameter(1, Types.INTEGER);
+            stmt.setInt(2, userID);
+            stmt.setString(3, newDisplayName);
+
+            stmt.executeUpdate();
+
+            int returnValue = stmt.getInt(1);
+
+            if (returnValue != 0) {
+                throw new SQLException("Change display name was not successful" + returnValue);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public boolean updateUsername(int userID, String newUsername) {
+        try {
+            Connection connection = dataSource.getConnection();
+            CallableStatement stmt = connection.prepareCall("{? = call UpdateUsername(@UserID = ?, @NewUserame = ?}");
+            stmt.registerOutParameter(1, Types.INTEGER);
+            stmt.setInt(2, userID);
+            stmt.setString(3, newUsername);
+
+            stmt.executeUpdate();
+
+            int returnValue = stmt.getInt(1);
+
+            if (returnValue != 0) {
+                throw new SQLException("Change Username was not Sucessful" + returnValue);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+//    public boolean updatePassword(int userID, String oldPass, String newPass) {
+//        try {
+//            Connection connection = dataSource.getConnection();
+//            CallableStatement stmt = connection.prepareCall("{? = call UpdatePassword(@UserID = ?, @NewPassword = ?}");
+//            stmt.registerOutParameter(1, Types.INTEGER);
+//            stmt.setInt(2, userID);
+//            stmt.setString(3, newUsername);
+//
+//            stmt.executeUpdate();
+//
+//            int returnValue = stmt.getInt(1);
+//
+//            if (returnValue != 0) {
+//                throw new SQLException("Change Username was not Sucessful" + returnValue);
+//            }
+//        } catch (Exception e) {
+//            // TODO: handle exception
+//        }
+//    }
+}
