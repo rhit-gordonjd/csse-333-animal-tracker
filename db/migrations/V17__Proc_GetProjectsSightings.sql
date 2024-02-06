29b8bf87631c96851a533bf41bc528969392195f
Wallace <wallacls@rose-hulman.edu>
2024-02-06 14:37:07 -0500

    Actually the stored procedure now

---
+++ db/migrations/V17__Proc_GetProjectsSightings.sql
@@ -0,0 +1,24 @@
+CREATE or ALTER PROCEDURE GetProjectSightings
+(
+	@ProjectID int
+)
+AS
+BEGIN
+
+	-- Validate parameters
+	IF @ProjectID IS NULL
+	BEGIN
+		RAISERROR('Parameter @ProjectID is null', 14, 1);
+		RETURN 1;
+	END
+	
+	SELECT S.ID as 'ID', S.Timestamp as 'Timestamp', S.Location.Lat as 'LocationLatitude', S.Location.Long as 'LocationLongitude', O.CommonName as 'CommonName', O.ScientificName as 'ScientificName', U.DisplayName as 'DisplayName'
+	FROM Project as P
+	JOIN Organism as O on P.ID = O.ProjectID
+	JOIN Sighting as S on O.ID = S.OrganismID
+	JOIN [User] as U on U.ID = S.UserID
+	WHERE P.ID = @ProjectID
+END
+GO
+
+GRANT EXECUTE ON GetProjectSightings TO AnimalTrackerApp
